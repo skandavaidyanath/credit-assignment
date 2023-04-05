dee52855cc14ae85a32476bcfcfd46f3b5a0a3e5
Akash Velu <akashvelu@berkeley.edu>
2023-04-05 15:18:39 -0700

    fixing skanda's bugs

--- src/wrappers/pw_wrapper.py
+++ src/wrappers/pw_wrapper.py
@@ -10,11 +10,15 @@ class PushWorldWrapper(gym.Wrapper):
     3) Allows modifications to info dict if required
     """
 
-    def __init__(pw_env, use_state=False):
-        super(PushWorldWrapper, self).__init__(env)
+    def __init__(self, pw_env, use_state=False):
+        super(PushWorldWrapper, self).__init__(pw_env)
 
-        self.env = env
+        self.env = pw_env
         self.use_state = use_state
+        if self.use_state:
+            state = self.reset()
+            bound = np.ones_like(state) * np.inf
+            self.observation_space = gym.spaces.Box(low=-1.0*np.inf, high=np.inf, shape=(len(state), ))
 
     def reset(self):
         observation, info = self.env.reset()
@@ -33,4 +37,5 @@ class PushWorldWrapper(gym.Wrapper):
             return observation, reward, done, info
 
     def _get_state(self):
-        return np.array(self.env._current_state)
+        state = np.array(self.env._current_state)
+        return state.flatten()
