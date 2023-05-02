fix input normalization for hca and dd models, add return normalization only option:

src/dualdice/dd_buffer.py
@@ -58,16 +58,21 @@ class DualDICEBuffer:
             val_dataloader = None
         return train_dataloader, val_dataloader
 
-    def get_input_stats(self):
+    def get_input_stats(self, normalize_returns_only):
         states = np.array(self.states)
         h_actions = np.array(self.h_actions).reshape((-1, self.action_dim))
         pi_actions = np.array(self.pi_actions).reshape((-1, self.action_dim))
+        actions = np.concatenate((h_actions, pi_actions), 0)
         returns = np.array(self.returns).reshape((-1, 1))
-        inp_h = np.concatenate((states, h_actions, returns), -1)
-        inp_pi = np.concatenate(
-            (states, pi_actions, np.zeros_like(self.returns).reshape((-1, 1))),
-            -1,
-        )
-        inp_h_mean, inp_h_std = np.mean(inp_h, 0), np.std(inp_h, 0)
-        inp_pi_mean, inp_pi_std = np.mean(inp_pi, 0), np.std(inp_pi, 0)
-        return inp_h_mean, inp_h_std, inp_pi_mean, inp_pi_std
+
+        if normalize_returns_only:
+            state_mean, state_std = np.zeros(states.shape[1]), np.ones(states.shape[1])
+            action_mean, action_std = np.zeros(actions.shape[1]), np.ones(actions.shape[1])
+        else:
+            state_mean, state_std = np.mean(states, 0), np.std(states, 0)
+            action_mean, action_std = np.mean(actions, 0), np.std(actions, 0)
+        return_mean, return_std = np.mean(returns, 0), np.std(returns, 0)
+
+        inp_mean = np.concatenate((state_mean, action_mean, return_mean), 0)
+        inp_std = np.concatenate((state_std, action_std, return_std), 0)
+        return inp_mean, inp_std

src/hca/buffer.py
@@ -108,12 +108,17 @@ class HCABuffer:
             val_dataloader = None
         return train_dataloader, val_dataloader
 
-    def get_input_stats(self):
+    def get_input_stats(self, normalize_returns_only):
         states = np.array(self.states)
         returns = np.array(self.returns).reshape((-1, 1))
-        inp_data = np.concatenate((states, returns), -1)
-        inp_mean = np.mean(inp_data, 0)
-        inp_std = np.std(inp_data, 0)
+        if normalize_returns_only:
+            state_mean = np.zeros(states.shape[1])
+            state_std = np.ones(states.shape[1])
+        else:
+            state_mean, state_std = np.mean(states, 0), np.std(states, 0)
+        return_mean, return_std = np.mean(returns, 0), np.std(returns, 0)
+        inp_mean = np.concatenate((state_mean, return_mean), 0)
+        inp_std = np.concatenate((state_std, return_std), 0)
         return inp_mean, inp_std
 
     def save_data(self, num_actions):

src/hca/model.py
@@ -24,7 +24,8 @@ class HCAModel(nn.Module):
         batch_size=64,
         lr=3e-4,
         device="cpu",
-        normalize_inputs=True,
+        normalize_inputs=False,
+        normalize_return_inputs_only=False,
         max_grad_norm=None,
         weight_training_samples=False,
         noise_std=None,
@@ -34,7 +35,8 @@ class HCAModel(nn.Module):
         self.state_dim = state_dim
         self.action_dim = action_dim if continuous else 1
         self.continuous = continuous
-        self.normalize_inputs = normalize_inputs
+        self.normalize_inputs = normalize_inputs or normalize_return_inputs_only
+        self.normalize_return_inputs_only = normalize_return_inputs_only
         self.max_grad_norm = max_grad_norm
         self.noise_std = noise_std
 
@@ -97,8 +99,8 @@ class HCAModel(nn.Module):
 
     def update_norm_stats(self, mean, std, refresh=True):
         if refresh:  # re-calculate stats each time we train model
-            self.input_mean = torch.from_numpy(mean).to(self.device)
-            self.input_std = torch.from_numpy(std).to(self.device)
+            self.input_mean = torch.from_numpy(mean).to(self.device).float()
+            self.input_std = torch.from_numpy(std).to(self.device).float()
         else:
             raise NotImplementedError
 
@@ -107,7 +109,8 @@ class HCAModel(nn.Module):
         forward pass a bunch of inputs into the model
         """
         if self.normalize_inputs:
-            inputs = (inputs - inputs.mean()) / (inputs.std() + 1e-6)
+            # if self.normalize_return_inputs_only==True, then the non-return input mean and std will be 0 and 1 resp.
+            inputs = (inputs - self.input_mean) / (self.input_std + 1e-6)
 
         out = self.net(inputs)
         if self.noise_std and add_noise: