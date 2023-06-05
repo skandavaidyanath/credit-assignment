8b23dcf28d68e8a1d08d780314c1d58a2d34fbf7
Akash Velu <akashvelu@berkeley.edu>
2023-06-05 14:24:38 -0700

    convert return samples to numpy in utils

--- src/utils.py
+++ src/utils.py
@@ -248,7 +248,7 @@ def get_psi_return_samples(sample_method, r_model, states, r_min, r_max):
         states = torch.from_numpy(np.stack(states).astype(np.float32)).to(
             r_model.device
         )
-        return_samples = r_model.forward(states, return_samples=True)
+        return_samples = r_model.forward(states, return_samples=True).detach().cpu().numpy()
     elif sample_method == "zeros":
         # This is theoretically incorrect.
         num_samples = len(states)
